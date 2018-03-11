f085bc986c83f37c1eee481f39e71ff384db5cf3
Phillip Webb <pwebb@pivotal.io>
2018-03-11 00:40:12 -0800

    Export metrics to Prometheus

--- src/main/java/com/example/webinar/WebinarApplication.java
+++ src/main/java/com/example/webinar/WebinarApplication.java
@@ -1,10 +1,20 @@
 package com.example.webinar;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebinarApplication {
+
+	@Bean
+	public MeterRegistryCustomizer<MeterRegistry> commonTags() {
+		return (registry) -> registry.config()
+				.commonTags("application", "webinar");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebinarApplication.class, args);
