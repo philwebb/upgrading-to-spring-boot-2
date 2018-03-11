1dd38828f7e0041e287ba5a0a15a56170780d957
Phillip Webb <pwebb@pivotal.io>
2018-03-11 00:38:52 -0800

    Configure Security

---
+++ src/main/java/com/example/cityapp/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.cityapp;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.requestMatchers(EndpointRequest.toAnyEndpoint()).authenticated()
+				.anyRequest().permitAll().and()
+			.formLogin().and()
+			.httpBasic();
+	}
+
+}
