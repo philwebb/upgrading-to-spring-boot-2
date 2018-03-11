cbbe0cc5ae704ba550bf23c77c86aa0b15e053f9
Phillip Webb <pwebb@pivotal.io>
2018-03-11 00:46:54 -0800

    Initial Application

---
+++ src/main/java/com/example/webinar/City.java
@@ -0,0 +1,32 @@
+package com.example.webinar;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class City {
+
+	@Id
+	private String id;
+
+	private String name;
+
+	private String country;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
