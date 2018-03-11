b2c4efb4b70e62ca0d354fa336052ece4e036351
Phillip Webb <pwebb@pivotal.io>
2018-03-11 00:38:06 -0800

    Fix Binder

--- src/main/java/com/example/webinar/DataImportConfiguration.java
+++ src/main/java/com/example/webinar/DataImportConfiguration.java
@@ -1,17 +1,15 @@
 package com.example.webinar;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.bind.PropertySourcesPropertyValues;
-import org.springframework.boot.bind.RelaxedDataBinder;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoOperations;
 
@@ -29,32 +27,14 @@ public class DataImportConfiguration {
 
 	private List<City> getCities() {
 		Properties yaml = loadCitiesYaml();
-		MutablePropertySources propertySources = new MutablePropertySources();
-		propertySources.addFirst(new PropertiesPropertySource("cities", yaml));
-		Cities cities = new Cities();
-		RelaxedDataBinder binder = new RelaxedDataBinder(cities);
-		binder.bind(new PropertySourcesPropertyValues(propertySources));
-		return cities.getCities();
+		MapConfigurationPropertySource source = new MapConfigurationPropertySource(yaml);
+		return new Binder(source).bind("cities", Bindable.listOf(City.class)).get();
 	}
 
 	private Properties loadCitiesYaml() {
 		YamlPropertiesFactoryBean properties = new YamlPropertiesFactoryBean();
 		properties.setResources(new ClassPathResource("cities.yml"));
 		return properties.getObject();
-	}
-
-	static class Cities {
-
-		private List<City> cities = new ArrayList<>();
-
-		public List<City> getCities() {
-			return cities;
-		}
-
-		public void setCities(List<City> cities) {
-			this.cities = cities;
-		}
-
 	}
 
 }
